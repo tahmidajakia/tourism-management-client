06a2fb74a88782186a5edbb3e5381ee5858e99db
Mst Tahmida Begum Jakia <devtahmidajakia@gmail.com>
2024-04-30 02:27:15 -0700

    created fotter section

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -17,7 +17,7 @@ const Footer = () => {
     <h6 className="footer-title">Company</h6> 
     <a className="link link-hover">About us</a>
     <a className="link link-hover">Contact</a>
-    <a className="link link-hover">Jobs</a>
+    <a className="link link-hover">Tour Information</a>
     <a className="link link-hover">Press kit</a>
   </nav> 
   <nav>
